b7ac81e1192fda697bb6fc47b8c55f2544b6d9e2
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2024-07-01 21:16:17 +0300

    voxelize volume

--- test.py
+++ test.py
@@ -1,19 +1,30 @@
+import h5py
 import trimesh
 
 
-def create_mesh() -> trimesh.Trimesh:
-    outer = trimesh.creation.box((0.2, 0.4, 0.5))
-
-    inner = trimesh.creation.box((0.05, 0.05, 0.05))
-    inner.apply_translation((0.05, 0.1, 0.2))
+def create_mesh(side: float, void: float) -> trimesh.Trimesh:
+    outer = trimesh.creation.box((side, side, side))
+    inner = trimesh.creation.box((void, void, void))
     inner.invert()
-
     return trimesh.util.concatenate(inner, outer)
 
 
+def voxelize(mesh: trimesh.Trimesh, voxsize: float) -> trimesh.voxel.VoxelGrid:
+    sphere: trimesh.primitives.Sphere = mesh.bounding_sphere
+    if not hasattr(sphere.primitive, "radius"):
+        raise RuntimeError("Sphere has no radius")
+    radius = int(sphere.primitive.radius / voxsize + 0.5)
+    volume = trimesh.voxel.creation.local_voxelize(mesh, sphere.center, voxsize, radius)
+    if volume is None:
+        raise RuntimeError("Could not voxelize mesh")
+    return volume
+
+
 def main() -> None:
-    mesh = create_mesh()
-    mesh.show()
+    mesh = create_mesh(1.0, 0.1)
+    volume = voxelize(mesh, 0.01)
+    with h5py.File("test.h5", "w") as f:
+        f.create_dataset("volume", data=volume.matrix, compression="gzip")
 
 
 if __name__ == "__main__":
