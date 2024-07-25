09d5fd70b612f769fcb6b0d61edccf9efa8aff5d
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2024-07-25 13:13:30 +0300

    angle dist

--- compare.py
+++ compare.py
@@ -1,10 +1,10 @@
+import argparse
 import datetime
-import h5py
 import json
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pathlib
+import roma
 import torch
 import torchio as tio
 import vedo
@@ -57,23 +57,15 @@ def generate_plane(scale: float) -> vedo.Mesh:
 
 def do_transform(
         obj: vedo.Mesh,
-        translation: tuple[float, float, float],
         rotation_axis: tuple[float, float, float],
         angle: float,
-        scale: float,
 ) -> None:
-    lt = (vedo.LinearTransform()
-          .scale(scale)
-          .rotate(angle, rotation_axis, rad=True)
-          .translate(translation)
-          )
+    lt = vedo.LinearTransform().rotate(angle, rotation_axis, rad=True)
     obj.apply_transform(lt)
-    # vedo.show(obj, axes=1).close()
 
 
 def do_voxelization(obj: vedo.Mesh, voxel_size: float, pad: int) -> np.ndarray:
     vol = obj.binarize((1, 0), spacing=(voxel_size, voxel_size, voxel_size))
-    # vedo.show(vol, axes=1).close()
     volume = vol.tonumpy().astype(np.float32)
     if pad > 0:
         volume = np.pad(volume, pad)
@@ -91,22 +83,16 @@ def add_noise(volume: np.ndarray, blur_size: int, noise: float) -> np.ndarray:
 
 
 def do_binarization(volume: np.ndarray) -> np.ndarray:
-    # plt.hist(volume.ravel(), bins="auto")
-    # plt.show()
     return volume > 0.5
 
 
 def bin_volume_to_mesh(volume: np.ndarray, voxel_size: float) -> vedo.Mesh:
     vol = vedo.Volume(volume)
-    # vedo.show(vol, axes=1).close()
     return vol.isosurface_discrete(1).scale(voxel_size)
 
 
 def do_icp(mesh: vedo.Mesh, target: vedo.Mesh) -> None:
     mesh.align_to(target, rigid=True)
-    mesh.c("red")
-    target.c("blue")
-    # vedo.show(mesh, target, axes=1).close()
 
 
 def calc_distance(m1: vedo.Mesh, m2: vedo.Mesh) -> tuple[float, float, float]:
@@ -115,20 +101,40 @@ def calc_distance(m1: vedo.Mesh, m2: vedo.Mesh) -> tuple[float, float, float]:
     return max(d1, d2), d1, d2
 
 
+def axis_angle_from_rotvec(rotvec) -> tuple[tuple[float, float, float], float]:
+    angle = rotvec.norm()
+    rotation_axis = (rotvec / angle).tolist()
+    return rotation_axis, float(angle)
+
+
+def calc_rot_distance(
+        rotvec,
+        restored: vedo.Mesh,
+        icp: vedo.Mesh,
+) -> tuple[float, tuple[float, float, float], float]:
+    rot = roma.rotvec_to_rotmat(rotvec).inverse()
+    icp_rot, _ = roma.rigid_points_registration(
+        torch.from_numpy(restored.vertices),
+        torch.from_numpy(icp.vertices),
+    )
+    icp_rotvec = roma.rotmat_to_rotvec(icp_rot)
+    icp_rot_axis, icp_rot_angle = axis_angle_from_rotvec(icp_rotvec)
+    theta = float(roma.rotmat_geodesic_distance(rot, icp_rot))
+    return theta, icp_rot_axis, icp_rot_angle
+
+
 def run(
-        translation: tuple[float, float, float],
-        rotation_axis: tuple[float, float, float],
-        angle: float,
+        rotvec,
+        voxel_size: float,
         blur_size: int,
         noise: float,
         result_path: pathlib.Path,
-) -> None:
-    scale = 1.0
-    voxel_size = 0.01
+) -> tuple[tuple[float, float, float], float]:
+    rotation_axis, angle = axis_angle_from_rotvec(rotvec)
 
     plane = generate_plane(0.1)
     transformed_plane = plane.copy()
-    do_transform(transformed_plane, translation, rotation_axis, angle, scale)
+    do_transform(transformed_plane, rotation_axis, angle)
 
     transformed_plane.write(str(result_path / "transformed_plane.stl"))
 
@@ -137,91 +143,87 @@ def run(
 
     plt.figure()
     plt.hist(noise_volume.ravel(), bins="auto")
-    plt.savefig(result_path / "histogram.png")
+    plt.savefig(result_path / "histogram.svg")
     plt.close()
 
     bin_volume = do_binarization(noise_volume)
 
-    with h5py.File(result_path / "voxels.h5", "w") as f:
-        f.create_dataset("volume", data=volume, compression="gzip")
-        f.create_dataset("noise_volume", data=noise_volume, compression="gzip")
-        f.create_dataset("bin_volume", data=bin_volume, compression="gzip")
-
     restored_plane = bin_volume_to_mesh(bin_volume, voxel_size)
-
     restored_plane.write(str(result_path / "restored_plane.stl"))
 
-    do_icp(restored_plane, plane)
+    restored_plane_icp = restored_plane.copy()
+    do_icp(restored_plane_icp, plane)
 
-    restored_plane.write(str(result_path / "restored_plane_icp.stl"))
+    restored_plane_icp.write(str(result_path / "restored_plane_icp.stl"))
 
-    dist = calc_distance(plane, restored_plane)
-    print(f"Max distance: {dist[0]:.3f}\nd1: {dist[1]:.3f}\nd2: {dist[2]:.3f}\n")
+    dist = calc_distance(plane, restored_plane_icp)
+
+    theta, icp_rot_axis, icp_rot_angle = calc_rot_distance(rotvec, restored_plane, restored_plane_icp)
 
     with open(result_path / "desc.json", "w", newline="\n") as f:
         desc = {
-            "scale": scale,
-            "translation": translation,
-            "rotation_axis": rotation_axis,
-            "angle": angle,
+            "rotation": {
+                "axis": rotation_axis,
+                "angle": angle,
+                "angle_deg": int(np.rad2deg(angle) + 0.5),
+            },
             "blur_size": blur_size,
             "noise": noise,
             "voxel_size": voxel_size,
             "distance": dist,
+            "rotation_dist": theta,
+            "rotation_dist_deg": int(np.rad2deg(theta) + 0.5),
+            "icp_rotation": {
+                "axis": icp_rot_axis,
+                "angle": icp_rot_angle,
+                "angle_deg": int(np.rad2deg(icp_rot_angle) + 0.5),
+            }
         }
         json.dump(desc, f, indent=4)
 
+    return dist, theta
+
+
+def hist(data: list[float], file_path: pathlib.Path, title: str) -> None:
+    plt.figure()
+    plt.title(title)
+    plt.hist(data, bins="auto")
+    plt.savefig(file_path)
+    plt.close()
+
 
 def main() -> None:
-    root_path = pathlib.Path().resolve()
-    tmp_path = root_path / "tmp"
-    data = [
-        {  # max=0.036 d1=0.036 d2=0.20
-            "translation": (2.0, 2.0, 1.0),
-            "rotation_axis": (math.sqrt(3)/3, math.sqrt(3)/3, math.sqrt(3)/3),
-            "angle": math.pi / 5,
-            "blur_size": 2,
-            "noise": 0.05,
-        },
-        {  # max=0.035 d1=0.035 d2=0.019
-            "translation": (10.0, 0.0, 0.0),
-            "rotation_axis": (math.sqrt(2)/2, math.sqrt(2)/2, 0),
-            "angle": math.pi / 3,
-            "blur_size": 2,
-            "noise": 0.05,
-        },
-        {  # max=1.563 d1=0.042 d2=1.563
-            "translation": (1.0, 2.0, 3.0),
-            "rotation_axis": (0.062280360609292984, 0.9798505306243896, 0.18977344036102295),
-            "angle": 1.2554593086242676,
-            "blur_size": 3,
-            "noise": 0.1,
-        },
-        {  # max=1.946 d1=0.086 d2=1.946
-            "translation": (5.0, 4.0, 2.0),
-            "rotation_axis": (-0.5714665055274963, -0.04317374899983406, 0.8194889426231384),
-            "angle": 0.6999582648277283,
-            "blur_size": 4,
-            "noise": 0.1,
-        },
-    ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num", default=10, type=int)
+    parser.add_argument("--vox-size", default=0.01, type=float)
+    parser.add_argument("--blur", default=2, type=int)
+    parser.add_argument("--noise", default=0.05, type=float)
+    parser.add_argument("result_dir", type=pathlib.Path)
+    args = parser.parse_args()
+    root_path: pathlib.Path = args.result_dir
+    root_path = root_path.resolve()
 
-    scale = 1.0
-    voxel_size = 0.01
-
-    # import roma
-    # rot = roma.random_rotvec()
-    # print(rot)
-    # angle = rot.norm()
-    # rotation_axis = (rot / angle).tolist()
-    # angle = float(angle)
-    # print(rotation_axis, angle)
-
-    for d in data:
+    num = args.num
+    n = 0
+    rot_vecs = roma.random_rotvec(num)
+    dists1 = []
+    dists2 = []
+    angles = []
+    for rot_vec in rot_vecs:
         now = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-        result_path = tmp_path / f"run_{now}"
+        result_path = root_path / f"run_{now}"
         result_path.mkdir(exist_ok=True, parents=True)
-        run(d["translation"], d["rotation_axis"], d["angle"], d["blur_size"], d["noise"], result_path)
+        dist, angle = run(rot_vec, args.vox_size, args.blur, args.noise, result_path)
+        dists1.append(dist[1])
+        dists2.append(dist[2])
+        angles.append(int(np.rad2deg(angle) + 0.5))
+        n += 1
+        print(f"Done {n}/{num}")
+    hist(dists1, root_path / "hist_dist1.svg", "plane to icp dist")
+    hist(dists2, root_path / "hist_dist2.svg", "icp to plane dist")
+    hist(angles, root_path / "hist_angles.svg", "angles hist")
+    plane = generate_plane(0.1)
+    plane.write(str(root_path / "plane.stl"))
 
 
 if __name__ == '__main__':
